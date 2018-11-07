7b5cf7deabb2c875a31637bc793891c60afe217b
Leandro Marchon <marchonleandro@gmail.com>
2018-11-07 11:38:54 -0200

    Adicionado nova configuração de rotas utilizando annotations.

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    /**
+     * @return Response
+     * @Route("hello-world")
+     */
     public function world()
     {
         return new Response(

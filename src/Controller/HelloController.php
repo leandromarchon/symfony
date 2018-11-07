20c799cf8de95cffde2d1bbaa5866753e065ed40
Leandro Marchon <marchonleandro@gmail.com>
2018-11-07 16:25:12 -0200

    Instalação dos bundles Form e Validator.

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Produto;
+use Doctrine\DBAL\Types\DecimalType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -47,5 +51,30 @@ class HelloController extends Controller
         $entityManager->flush();
 
         return new Response("O produto #".$produto->getId()." - ".$produto->getNome()." foi adicionado com sucesso.");
+    }
+
+    /**
+     * @return Response
+     * @Route("formulario")
+     */
+    public function formulario(Request $request)
+    {
+        $produto = new Produto();
+
+        $form = $this->createFormBuilder($produto)
+            ->add('nome', TextType::class)
+            ->add('preco', TextType::class)
+            ->add('enviar', SubmitType::class, ['label' => 'Salvar'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            return new Response("Formulário válido.");
+        }
+
+        return $this->render("hello/formulario.html.twig", [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Entity/Produto.php
+++ src/Entity/Produto.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProdutoRepository")
@@ -19,12 +20,14 @@ class Produto
     /**
      * @var string
      * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
      */
     private $nome;
 
     /**
      * @var float
      * @ORM\Column(type="decimal", scale=2)
+     * @Assert\NotBlank()
      */
     private $preco;
 

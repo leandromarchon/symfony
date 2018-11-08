b5af776ab33d7c2bdc7e8c59b2f4b9b36b91286b
Leandro Marchon <marchonleandro@gmail.com>
2018-11-08 15:59:55 -0200

    Criando um CRUD de produtos - Preparando o formulário.

--- src/Controller/ProdutoController.php
+++ src/Controller/ProdutoController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Produto;
+use App\Form\ProdutoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProdutoController extends AbstractController
 {
@@ -18,6 +21,21 @@ class ProdutoController extends AbstractController
 
         return $this->render("produto/index.html.twig", array(
             "produtos" => $produtos
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/produto/cadastrar", name="cadastrar_produto")
+     */
+    public function create(Request $request)
+    {
+        $produto = new Produto();
+        $form = $this->createForm(ProdutoType::class, $produto);
+        $form->handleRequest($request);
+        
+        return $this->render("produto/create.html.twig", array(
+            'form' => $form->createView()
         ));
     }
 }
---
+++ src/Form/ProdutoType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProdutoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nome', TextType::class, array(
+                'label' => "Nome",
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('preco', TextType::class, array(
+                'label' => 'Preço',
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('descricao', TextareaType::class, array(
+                'label' => 'Descrição',
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('enviar', SubmitType::class, array(
+                'label' => 'Cadastrar',
+                'attr' => array(
+                    'class' => 'btn btn-primary mt-5'
+                )
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
